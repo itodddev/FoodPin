c0ad97c8cdde03eb611df5d66013cebd988b0aa7
Todd James <todd@itodd.co>
2022-07-21 13:22:37 -0400

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Todd James on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
